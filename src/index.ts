454fb388230d6a981c310ca963b14469c7254016
Bombeta <paullo_jf@hotmail.com>
2023-07-17 17:12:47 -0300

    criando endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log("Hello World!!")
+import { PrismaClient } from '@prisma/client';
+import dotenv from 'dotenv'
+import express, {Express, Request, Response} from 'express';
+
+dotenv.config();
+
+const app: Express= express();
+const port = process.env.PORT || 5000;
+const prisma = new PrismaClient();
+
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({ message: 'Hello World!'});
+})
+
+app.get('/snacks', async (req: Request, res: Response) => {
+   
+    const { snack } = req.query
+
+   if(!snack)  return res.status(400).send({ error: "Snack is required"})
+
+   //res.json(snack)
+
+   const snacks = await prisma.snack.findMany({
+        where: {
+            snack: {
+                equals: snack as string
+            }
+        }
+   })
+
+   res.send(snacks)
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
+
